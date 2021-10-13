352cfa64816132502eebc8d996484e5ece622471
LocoMH <till.rasche@gmail.com>
2021-10-13 16:29:41 +0200

    added icons

--- sources/loader/setup.py
+++ sources/loader/setup.py
@@ -6,7 +6,14 @@ import os
 sys.argv.append('py2exe')
 
 setup(
-    options={'py2exe': {'bundle_files': 1, 'compressed': True}},
-    windows=[{'script': "loader.py"}],
+    options={'py2exe': {'bundle_files': 1,
+                        'compressed': True, }},
+    windows=[
+        {
+            'script': "loader.py",
+            "icon_resources": [(1, "../frontend/tm.ico")],
+            "dest_base": "loader"
+        }
+    ],
     zipfile=None,
 )
